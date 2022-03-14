2834748d88e9f254d3e920c6d73d6794dd382f78
Leon Suarez <leonsua@gmail.com>
2022-03-14 19:26:06 -0400

    added knews and style card

--- mapanimation.js
+++ mapanimation.js
@@ -3,68 +3,68 @@ var marker;
 mapboxgl.accessToken = 'pk.eyJ1IjoibGVvbnN1YXJlejEwIiwiYSI6ImNsMG4wajNkMTEwNWEzY28zMnNzZGtyamIifQ.fegQJGn9sTp4fAdo7NTBZw';
 
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [-118.183883, 33.955836 ],
-    zoom: 10
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-118.183883, 33.955836],
+  zoom: 10
 });
 
-function showMarker () {
+function showMarker() {
   marker = new mapboxgl.Marker()
-  .setLngLat([-118.183883, 33.955836])
-  .addTo(map);
+    .setLngLat([-118.183883, 33.955836])
+    .addTo(map);
 }
 
 map.on('load', () => {
   map.addSource('route', {
-  'type': 'geojson',
-  'data': {
-  'type': 'Feature',
-  'properties': {},
-  'geometry': {
-  'type': 'LineString',
-  'coordinates': [
-    [-118.187199, 33.921381],
-    [-118.183883, 33.955836],
-    [-118.182576, 33.933707],
-    [-118.190169, 33.914194],
-    [-118.193283, 33.906704],
-    [-118.185732, 33.925698],
-    [-118.191828, 33.910166],
-    [-118.184687, 33.958259],
-    [-118.186841, 33.965883],
-    [-118.188167, 33.919017],
-  ]
-  }
-  }
+    'type': 'geojson',
+    'data': {
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+        'type': 'LineString',
+        'coordinates': [
+          [-118.187199, 33.921381],
+          [-118.183883, 33.955836],
+          [-118.182576, 33.933707],
+          [-118.190169, 33.914194],
+          [-118.193283, 33.906704],
+          [-118.185732, 33.925698],
+          [-118.191828, 33.910166],
+          [-118.184687, 33.958259],
+          [-118.186841, 33.965883],
+          [-118.188167, 33.919017],
+        ]
+      }
+    }
   });
   map.addLayer({
-  'id': 'route',
-  'type': 'line',
-  'source': 'route',
-  'layout': {
-  'line-join': 'round',
-  'line-cap': 'round'
-  },
-  'paint': {
-  'line-color': 'blue',
-  'line-width': 8
-  }
+    'id': 'route',
+    'type': 'line',
+    'source': 'route',
+    'layout': {
+      'line-join': 'round',
+      'line-cap': 'round'
+    },
+    'paint': {
+      'line-color': 'blue',
+      'line-width': 8
+    }
   });
-  });
+});
 
-  const busStops = [
-    [-118.187199, 33.921381],
-    [-118.183883, 33.955836],
-    [-118.182576, 33.933707],
-    [-118.190169, 33.914194],
-    [-118.193283, 33.906704],
-    [-118.185732, 33.925698],
-    [-118.191828, 33.910166],
-    [-118.184687, 33.958259],
-    [-118.186841, 33.965883],
-    [-118.188167, 33.919017],
-  ];
+const busStops = [
+  [-118.187199, 33.921381],
+  [-118.183883, 33.955836],
+  [-118.182576, 33.933707],
+  [-118.190169, 33.914194],
+  [-118.193283, 33.906704],
+  [-118.185732, 33.925698],
+  [-118.191828, 33.910166],
+  [-118.184687, 33.958259],
+  [-118.186841, 33.965883],
+  [-118.188167, 33.919017],
+];
 
 var counter = 0;
 
@@ -76,13 +76,13 @@ function move() {
     move();
   }, 1000);
 }
- 
+
 // Add the control to the map.
 map.addControl(
-new MapboxGeocoder({
-accessToken: mapboxgl.accessToken,
-mapboxgl: mapboxgl
-})
+  new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl
+  })
 );
 
 //'mapbox://styles/mapbox/dark-v10'
@@ -92,7 +92,8 @@ function sucessHandler(data) {
   const dataObj = data;
   const weatherDiv = document.getElementById('temp');
   const weatherFragment = `
-    <h3>Weather</h3>
+    <h5 class='weather-title'>Weather</h5>
+    <hr class='division'/>
     <h5>${dataObj.name}</h5>
     <div class='center-img'>
     <img 
@@ -112,7 +113,7 @@ function tempToF(kelvin) {
   return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
 }
 // Fetch weather data
-async function fetchWeatherData () {
+async function fetchWeatherData() {
   const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&appid=89e5d770eac3d99dee8786f4791aa5fb';
   const response = await fetch(url);
   const data = await response.text();
@@ -123,19 +124,39 @@ async function fetchWeatherData () {
 //===========Weather End========//
 //===========Knews API begin========//
 
-function knewsSuccessHandler (data) {
+function knewsSuccessHandler(data) {
   const knewsObj = data.results;
-  const knewsOneDiv = document.getElementById('knewsOne');
-  const knewsOneFragment = `
-    <h3>Knews</h3>
-    <p>${knewsObj[0].title}</p>
-  `
-  console.log(knewsObj);
-  knewsOneDiv.innerHTML = knewsOneFragment;
+  let counter = 0;
+  setInterval(() => {
+    if (counter <= knewsObj.length) {
+      counter++;
+      let img = ''
+      if (knewsObj[counter].image_url === null) {
+        img = 'coming-soon.jpeg'
+      } else {
+        img = knewsObj[counter].image_url
+      }
+  
+      const knewsOneDiv = document.getElementById('knewsOne');
+      const knewsOneFragment = `
+      <a href='${knewsObj[counter].link}' class='link-to' target="_blank">
+      <figure class='card'>
+      <img src='${img}'/>
+      <figcaption class='caption'>
+      <span class='caption-title'>${(knewsObj[counter].title).slice(0, 20)}</span>
+      </figcaption>
+      </figure>
+      </a>
+      `
+      console.log(knewsObj);
+      knewsOneDiv.innerHTML = knewsOneFragment;
+    }
+  }, 5000);
+  counter = 0;
 }
 
 async function fetchKnewsData() {
-  const url = 'https://newsdata.io/api/1/news?apikey=pub_5471c2f13f0b107554e22983dc66674168b5&country=us';
+  const url = 'https://newsdata.io/api/1/news?apikey=pub_5471c2f13f0b107554e22983dc66674168b5&country=us&language=en';
   const response = await fetch(url);
   const data = await response.text();
   const obj = JSON.parse(data);
